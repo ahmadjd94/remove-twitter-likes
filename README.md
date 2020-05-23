43fd0ec013005ea866c38ec5222823ae20165c8d
banana <ahmadjd94@gmail.com>
2020-05-23 04:23:30 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # remove-twitter-likes
 unlike tweets on twitter for a profile, given the username and the password
+
+###steps
+1) Change the username and the password within the code
+2) download geckodriver, extract it in the same directory of the code
+4) run ```pip3 install -r requirements.txt```
+3) run ``python unlike.py``
+
---
+++ unlike.py
@@ -0,0 +1,49 @@
+import time
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+drive = webdriver.Firefox(executable_path="/Users/banana/PycharmProjects/unlike_this/geckodriver")
+
+drive.get("https://twitter.com/login")
+
+login = drive.find_element_by_xpath("""//input[@type="text"]""")
+# login.click()
+login.send_keys("")  # Insert your username here
+
+password = drive.find_element_by_xpath("""//input[@type="password"]""")
+
+password.send_keys("")  # Insert your username here
+
+drive.find_element_by_xpath("""//div[@data-testid="LoginForm_Login_Button"]""").click()
+
+drive.get("https://twitter.com/ojdjo/likes")
+
+time.sleep(4)
+
+current_len = 10
+unlikes = 0
+are_you_sure = False
+while current_len or not are_you_sure:
+    for i in range(current_len):
+        try:
+            target_button = drive.find_element_by_xpath("""//div[@data-testid="unlike"]""")
+            target_button.click()
+            unlikes += 1
+        except NoSuchElementException:
+            are_you_sure = True
+            drive.get("https://twitter.com/ojdjo/likes")
+            time.sleep(3)
+
+    try:
+        drive.get("https://twitter.com/ojdjo/likes")
+
+        time.sleep(2)
+
+        current_len = len(drive.find_elements_by_xpath("""//div[@data-testid="unlike"]"""))
+
+    except Exception as exc1:
+        break
+print(f"total_unlikes{unlikes}")
+
